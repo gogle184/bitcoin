9786141bca99953be49ff5da2372bfa7cadbb8ac
gogle184 <gogle184@gmail.com>
2025-03-11 21:51:30 +0900

    fix: construct

--- lesson.go
+++ lesson.go
@@ -3,26 +3,31 @@ package main
 import "fmt"
 
 type Vertex struct {
-  X int
-  Y int
+  x, y int
 }
 
 func (v Vertex) Area() int {
-  return v.X * v.Y
+  return v.x * v.y
 }
 
 func (v *Vertex) Scale(i int) {
-  v.X = v.X * i
-  v.Y = v.Y * i
+  v.x = v.x * i
+  v.y = v.y * i
 }
 
-func Area(v Vertex) int {
-  return v.X * v.Y
+// func Area(v Vertex) int {
+//   return v.x * v.y
+// }
+
+func New(x, y int) *Vertex {
+  return &Vertex{x, y}
 }
 
 func main() {
-  v := Vertex{3,4}
-  fmt.Println(Area(v))
+  // v := Vertex{3,4}
+  // fmt.Println(Area(v))
+  v := New(3,4)
+
   fmt.Println(v.Area())
 
   v.Scale(10)

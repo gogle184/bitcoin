5f8a3b34ad168c2baa10750ffe88e6c5742f6504
gogle184 <gogle184@gmail.com>
2025-03-11 22:12:14 +0900

    embeded

--- lesson.go
+++ lesson.go
@@ -15,21 +15,37 @@ func (v *Vertex) Scale(i int) {
   v.y = v.y * i
 }
 
+type Vertex3D struct {
+  Vertex
+  z int
+}
+
+func (v Vertex3D) Area3D() int {
+  return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+  v.x = v.x * i
+  v.y = v.y * i
+  v.z = v.z * i
+}
+
 // func Area(v Vertex) int {
 //   return v.x * v.y
 // }
 
-func New(x, y int) *Vertex {
-  return &Vertex{x, y}
+func New(x, y, z int) *Vertex3D {
+  return &Vertex3D{Vertex{x, y}, z}
 }
 
 func main() {
   // v := Vertex{3,4}
   // fmt.Println(Area(v))
-  v := New(3,4)
+  v := New(3,4, 5)
 
   fmt.Println(v.Area())
 
   v.Scale(10)
   fmt.Println(v.Area())
+  fmt.Println(v.Area3D())
 }

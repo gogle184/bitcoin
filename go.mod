90ce8edf4a209552c0d5f25ce32334c30ad23fa6
gogle184 <gogle184@gmail.com>
2025-03-16 18:32:43 +0900

    time

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module goproject
 
 go 1.24.0
+
+require (
+	github.com/markcheno/go-quote v0.0.0-20250303011253-4b4b2aed5a7a // indirect
+	github.com/markcheno/go-talib v0.0.0-20250114000313-ec55a20c902f // indirect
+)
--- lesson.go
+++ lesson.go
@@ -1,40 +1,12 @@
 package main
 
 import (
-    "fmt"
-
+	"fmt"
+	"time"
 )
 
-func producer(first chan int){
-  defer close(first)
-  for i := 0; i < 10; i++{
-    first <- i
-  }
-}
-
-func multi2(first chan int, second chan int){
-  defer close(second)
-  for i := range first{
-    second <- i * 2
-  }
-}
-
-func multi3(second chan int, third chan int){
-  defer close(third)
-  for i := range second{
-    third <- i * 4
-  }
-}
-
-func main(){
-  first := make(chan int)
-  second := make(chan int)
-  third := make(chan int)
-
-  go producer(first)
-  go multi2(first, second)
-  go multi3(second, third)
-  for result := range third{
-    fmt.Println(result)
-  }
+func main() {
+	t := time.Now()
+	fmt.Println(t)
+	fmt.Println(t.Format(time.RFC3339))
 }

ccb81448e31caf795ed0689c3c4ed64ef52af261
gogle184 <gogle184@gmail.com>
2025-03-17 19:26:54 +0900

    save&load

--- lesson.go
+++ lesson.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
 	"fmt"
+	"os"
 )
 
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error){
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func main() {
-	const apiKey = "1234567890"
-	const apiSecret = "0987654321"
+	p1 := &Page{Title: "test", Body: []byte("This is a test page.")}
+	p1.save()
 
-	h := hmac.New(sha256.New, []byte(apiSecret))
-	fmt.Println(h)
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
